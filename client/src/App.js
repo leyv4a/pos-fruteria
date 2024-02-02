d8345c372869b9647e9e577c05de90d114405e91
leyv4a <gabriel.leyva228902@potros.itson.edu.mx>
2024-02-02 14:42:00 -0700

    CRUD EN CLIENTE Y SERVIDOR COMPLETO

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,70 @@
 import './App.css';
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect} from 'react';
+import Axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
-  const [producto, setProducto] = useState("");
-  const [cantidad, setCantidad] = useState("");
-  const [precio, setPrecio] = useState(0);
+  const [nombre, setNombre] = useState("");
+  const [stock, setStock] = useState("");
+  const [precio, setPrecio] = useState("");
   const [categoria, setCategoria] = useState("");
+  const [id, setId] = useState("");
 
+  const [editar, setEditarProductos] = useState(false);
   const [listaProductos, setProductos] = useState([]);
 
   const add = () => {
-   axios.post("http://localhost:3001/productos",{
-    producto: producto,
-    cantidad: cantidad,
-    precio: precio,
-    categoria: categoria
-   }).then(()=>{getProductos(); alert("Producto registrado con exito")});
+   Axios.post("http://localhost:3001/productos",{
+    nombre:nombre,
+    stock:stock,
+    precio:precio,
+    categoria:categoria
+   }).then(()=>{getProductos(); alert("Producto registrado con exito")}); limpiarCampos();
   }
 
-  const getProductos = () => {
-    axios.get("http://localhost:3001/productos").then((res)=>{
+  const actualizar = () => {
+    Axios.put("http://localhost:3001/productos", {
+      id: id,
+      nombre: nombre,
+      stock: stock,
+      precio: precio,
+      categoria: categoria
+    }).then(()=>{getProductos(); alert("Producto actualizado con exito")}); limpiarCampos();
+  }
+
+
+  const getProductos = (val) => {
+    Axios.get("http://localhost:3001/productos").then((res)=>{
       setProductos(res.data);
     });
+  }
+
+  useEffect(() => {
+    getProductos();
+  }, []);
+
+  const editarProductos = (val) => {
+    setEditarProductos(true);
+    setNombre(val.nombre);
+    setStock(val.stock);
+    setPrecio(val.precio);
+    setCategoria(val.categoria);
+    setId(val.id);
+  }
+
+  const eliminarProducto = (val) =>{
+    Axios.delete(`http://localhost:3001/productos/${val.id}`).then((res)=>{
+      getProductos(); alert("Producto eliminado con exito");
+    })
+  }
+
+  const limpiarCampos = () =>{
+    setNombre("");
+    setStock("");
+    setPrecio("");
+    setCategoria("");
+    setId("");
   }
 
   return (
@@ -33,48 +73,57 @@ function App() {
         <div className='row '> 
         <div className='col-md-6 border-end '>
           <form className='p-5'>
-            <input placeholder="Productos" className='form-control mb-3' onChange={(e)=>{setProducto(e.target.value)}} type='text'/>
-            <input placeholder="Cantidad" className='form-control mb-3' onChange={(e)=>{setCantidad(e.target.value)}} type='text'></input>
-            <input placeholder="Precio" className='form-control mb-3' onChange={(e)=>{setPrecio(e.target.value)}} type='text'></input>
-            <input placeholder="Categoria" className='form-control mb-3' onChange={(e)=>{setCategoria(e.target.value)}}></input>
-            <button className='btn btn-light text-success  px-5' onClick={add}>Aceptar</button>
+            <input placeholder="Productos" className='form-control mb-3' value={nombre} onChange={(e)=>{setNombre(e.target.value)}} type='text'/>
+            <input placeholder="Cantidad" className='form-control mb-3' value={stock} onChange={(e)=>{setStock(e.target.value)}} type='text'></input>
+            <input placeholder="Precio" className='form-control mb-3' value={precio} onChange={(e)=>{setPrecio(e.target.value)}} type='text'></input>
+            <input placeholder="Categoria" className='form-control mb-3' value={categoria} onChange={(e)=>{setCategoria(e.target.value)}}></input>
+            <div>
+              {
+                editar? 
+                        <div>
+                          <button type='button' className='btn btn-info text-white me-3 px-5' onClick={actualizar}>Actualizar</button>  
+                          <button type='button' className='btn btn-danger text-white px-5' onClick={()=>{setEditarProductos(false); limpiarCampos();}}>Cancelar</button>
+                        </div> 
+                     : 
+                        <button type='button' className='btn btn-light text-success  px-5' onClick={add}>Agregar</button>
+              }
+            </div>
           </form>
         </div>
         <div className='col-md-6 p-5'>
-          <table class="table table-striped table-success">
+          <table className="table table-striped table-success">
             <thead>
               <tr>
-                <th scope="col">#</th>
                 <th scope="col">Nombre</th>
                 <th scope="col">Cantidad</th>
                 <th scope="col">Precio</th>
                 <th scope="col">Categoria</th>
+                <th scope="col">Acciones</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-                <td>@mdo</td>
-              </tr>
+            {
+          listaProductos.map((val, key) => {
+            return <tr key={val.id}>
+                      <td>{val.nombre}</td>
+                      <td>{val.stock}</td>
+                      <td>{val.precio}</td>
+                      <td>{val.categoria}</td>
+                      <td>
+                          <div className="btn-group" role="group" aria-label="Basic mixed styles example">
+                            <button type="button" onClick={() => editarProductos(val)} className="btn btn-warning">Actualizar</button>
+                            <button type="button" className="btn btn-danger" onClick={()=>{eliminarProducto(val)}}>Eliminar</button>
+                          </div>
+                      </td>
+                   </tr>
+          })
+        }
             </tbody>
           </table>
         </div>
         </div>
       </div>
-      <div className='Listar'>
-        <button onClick={getProductos}>Listar</button>
-
-        {
-          listaProductos.map((val, key) => {
-            return <div>{val.nombre}</div>
-          })
-        }
-      </div>
     </div>
-    
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -15,22 +15,39 @@ const db = mysql.createConnection({
 });
 
 app.post('/productos', (req, res) => {
-    const producto = req.body.producto;
-    const cantidad = req.body.cantidad;
+    const nombre = req.body.nombre;
+    const stock = req.body.stock;
     const precio = req.body.precio;
     const categoria = req.body.categoria;
    
 
-    db.query("INSERT INTO stock(nombre, precio, categoria, stock) VALUES (?, ?,?,?)",[producto, precio,categoria,cantidad],
+    db.query("INSERT INTO stock(nombre, precio, categoria, stock) VALUES (?, ?,?,?)",[nombre, precio,categoria,stock],
     (err,result) => err ? console.log(err) : res.send("Producto agregado correctamente")   
     );
 });
 
 app.get('/productos', (req, res) => {
-    db.query("SELECT nombre, precio, categoria, stock FROM stock",
+    db.query("SELECT id, nombre, precio, categoria, stock FROM stock",
     (err,result) => err ? console.log(err) : res.send(result)   
     );
 });
+
+app.put('/productos', (req, res) =>{
+    const id = req.body.id;
+    const nombre = req.body.nombre;
+    const precio = req.body.precio;
+    const categoria = req.body.categoria;
+    const stock = req.body.stock;
+
+    db.query("UPDATE stock SET nombre =?, precio =?, categoria =?, stock =? WHERE (id =?)",[nombre, precio,categoria,stock,id],
+    (err,result) => err? console.log(err) : res.send(result)   
+    );
+})
+
+app.delete('/productos/:id', (req, res) =>{
+    const id = req.params.id;
+    db.query('DELETE FROM stock WHERE id = ?',[id], (err,result) => err? console.log(err): res.send(result));
+})
 
 app.listen(3001, () => {
     console.log('Server running on port 3001');
